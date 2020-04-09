1784e11090b83be9d832a989aecd122291331222
varshajha28 <varsha.jha@gmail.com>
2020-04-09 12:03:02 -0700

    Adding geojson logic

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 // Create the map object with center and zoom level.
 let map = L.map('mapid').setView([30,30 ], 2);
 // Accessing the airport GeoJSON URL
-//let airportData = "https://raw.githubusercontent.com/varshajha28/Mapping_Earthquakes/master/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/varshajha28/Mapping_Earthquakes/master/majorAirports.json";
 // // Add GeoJSON data.
 // let sanFranAirport =
 // {"type":"FeatureCollection","features":[{
